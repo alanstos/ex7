fbe1c7c097ce92de1fb796d9e947bb7dd343de50
alanstos <alanstos@gmail.com>
2015-09-05 17:06:31 -0300

    '...'

--- app/controllers/indexC.js
+++ app/controllers/indexC.js
@@ -1,20 +1,9 @@
 
-
-
 var express = require('express');
 var router = express.Router();
-
-
 var Log = require('../models/Log');
 
-
 var app = express();
-
-
-
-
-
-
 
 /* GET home page. */
 exports.raiz = function(req, res, next) {
@@ -38,38 +27,36 @@ exports.raiz = function(req, res, next) {
 		res.render('index', { title: 'Express', logss : logs, appnome : 'valor qualquer' });
 	});		
 	
-  console.log('----------variavel logss : ' , logss);
-  
-  
+  console.log('----------variavel logss : ' , logss); 
   
 };
 
-router.get('/manter', function(req, res, next) {
+//manter
+exports.manter = function(req, res, next) {
+	console.log("ENTROU NO MANTER NOVO.... :");
+	var id = req.query.id;
+	 
+	Log.findOne({ where: {id: id} }).then(function(log) {
+	  // project will be the first entry of the Projects table with the title 'aProject' || null
+	  console.log("findOnde = " , log.nome);
+	  res.render('manter', { id: id,  nome : log.nome });
+	})
+	 
+	Log.findById(id).then(function(log) {
+	  // project will be an instance of Project and stores the content of the table entry
+	  // with id 123. if such an entry is not defined you will get null
+	  
+	  
+	})
+  
+};
 
- var id = req.query.id;
- 
- Log.findOne({ where: {id: id} }).then(function(log) {
-  // project will be the first entry of the Projects table with the title 'aProject' || null
-  console.log("findOnde = " , log.nome);
-  res.render('manter', { id: id,  nome : log.nome });
-})
- 
- Log.findById(id).then(function(log) {
-  // project will be an instance of Project and stores the content of the table entry
-  // with id 123. if such an entry is not defined you will get null
-  
-  
-})
-  
 
-router.post('/merge', function(req, res, next) {
+//merge
+exports.merge =  function(req, res, next) {
+	console.log("ENTROU NO merge novo.... :");
 	var nome = req.body.nome;
 	var id = req.body._id;
-    console.log("log.... merge = ", nome, " id=",id);
+	console.log("log.... merge = ", nome, " id=",id);
 	res.render('manter', { id: id,  nome : nome ,msg : "Enviado com sucesso"});
-})
-  
-  
-});
-
-
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -12,69 +12,12 @@ console.log('nova forma.....');
 console.log('controllers.....');
 
 
-//var Sequelize = require('sequelize');
-
-//var sequelize = new Sequelize('jdbc:mysql://localhost:3306/recorrame');
-
-/* var sequelize = new Sequelize(config.banco_name, config.user, config.user, {
-  host: 'localhost',
-  dialect: 'mysql',
-
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-
-}); */
-
-// Definir o meu model de Log
-//var Log = sequelize.define('re_log', {
- // nome: {
-//    type: Sequelize.STRING,
-//    field: 'nome' // caso queira renomear
-//  },
-//  descricao: {
-//    type: Sequelize.STRING
-//  },
-//  data: {
-//    type: Sequelize.DATE
-//  }  
-//}, {
-//  freezeTableName: false // usar o nome da tabela o mesmo nome definido para o model 'usuario'
-//});
-
-
-/* GET home page. */
+/* ROTAS */
 router_.get('/', indexC.raiz);
 
-router_.get('/manter', function(req, res, next) {
+router_.get('/manter', indexC.manter);
 
- var id = req.query.id;
- 
- Log.findOne({ where: {id: id} }).then(function(log) {
-  // project will be the first entry of the Projects table with the title 'aProject' || null
-  console.log("findOnde = " , log.nome);
-  res.render('manter', { id: id,  nome : log.nome });
-})
- 
- Log.findById(id).then(function(log) {
-  // project will be an instance of Project and stores the content of the table entry
-  // with id 123. if such an entry is not defined you will get null
+router_.post('/merge', indexC.merge)
   
-  
-})
-  
-
-router_.post('/merge', function(req, res, next) {
-	var nome = req.body.nome;
-	var id = req.body._id;
-    console.log("log.... merge = ", nome, " id=",id);
-	res.render('manter', { id: id,  nome : nome ,msg : "Enviado com sucesso"});
-})
-  
-  
-});
-
 
 module.exports = router_;
